7cd430ebe22f3746aed462868bc1da5de7353899
Fabian Piper <fabianpiper@web.de>
2021-11-22 12:35:42 +0100

    Shared Social Icons

--- components/shared/SocialShare.vue
+++ components/shared/SocialShare.vue
@@ -10,72 +10,20 @@
         class='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 container px-4 mx-auto lg:max-w-6xl'>
         <ul class='flex items-center justify-center space-x-8'>
           <li>
-
-            <a class='flex items-center justify-center w-12 h-12 rounded-md bg-blue' href='/'
-               target='_blank'
-               rel='noopener noreferrer'>
-              <svg xmlns='http://www.w3.org/2000/svg' class='w-full h-full' viewBox='0 0 24 24' fill='none'
-                   stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'>
-                <rect x='2' y='2' width='20' height='20' rx='5' ry='5' />
-                <path d='M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z' />
-                <line x1='17.5' y1='6.5' x2='17.51' y2='6.5' />
-              </svg>
-              <span class='sr-only'>Instagram</span>
-            </a>
+            <Instagram class='!w-12 !h-12 bg-blue'></Instagram>
           </li>
           <li>
-            <a class='flex items-center justify-center w-12 h-12 border border-yellow border-4 bg-blue rounded-md p-2'
-               href='/'
-               target='_blank'
-               rel='noopener noreferrer'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='17' height='17' viewBox='0 0 17 17' data-v-31afccdd=''>
-                <path
-                  d='M0.698 5.823h3.438v10.323h-3.438v-10.323zM2.438 0.854c-1.167 0-1.938 0.771-1.938 1.782 0 0.989 0.74 1.781 1.896 1.781h0.021c1.198 0 1.948-0.792 1.938-1.781-0.011-1.011-0.74-1.782-1.917-1.782zM12.552 5.583c-1.829 0-2.643 1.002-3.094 1.709v-1.469h-3.427c0 0 0.042 0.969 0 10.323h3.427v-5.761c0-0.312 0.032-0.615 0.114-0.843 0.251-0.615 0.812-1.25 1.762-1.25 1.238 0 1.738 0.948 1.738 2.333v5.521h3.428v-5.917c0-3.167-1.688-4.646-3.948-4.646z'
-                  fill='currentColor'></path>
-              </svg>
-              <span class='sr-only'>LinkedIn</span>
-            </a>
+            <Facebook class='!w-12 !h-12 !border-4 !p-3 bg-blue'></Facebook>
           </li>
           <li>
-            <a class='flex items-center justify-center w-12 h-12 border border-yellow border-4 bg-blue rounded-md p-3'
-               href='/'
-               target='_blank'
-               rel='noopener noreferrer'>
-              <svg version='1.1' xmlns='http://www.w3.org/2000/svg' width='17' height='17' class='w-full h-full'
-                   viewBox='0 0 17 17'
-              >
-                <path
-                  d='M12.461 5.57l-0.309 2.93h-2.342v8.5h-3.518v-8.5h-1.753v-2.93h1.753v-1.764c0-2.383 0.991-3.806 3.808-3.806h2.341v2.93h-1.465c-1.093 0-1.166 0.413-1.166 1.176v1.464h2.651z'
-                  fill='currentColor' />
-              </svg>
-              <span class='sr-only'>Facebook</span>
-            </a>
+            <Linkedin class='!w-12 !h-12 !border-4 !p-2 bg-blue'></Linkedin>
           </li>
           <li>
-            <a class='flex items-center justify-center w-12 h-12' href='/'
-               target='_blank'
-               rel='noopener noreferrer'>
-              <svg xmlns='http://www.w3.org/2000/svg' class='w-full h-full' viewBox='-32 0 512 512'
-                   fill='currentColor'>
-                <path
-                  d='M267.429 488.563C262.286 507.573 242.858 512 224 512c-18.857 0-38.286-4.427-43.428-23.437C172.927 460.134 160 388.898 160 355.75c0-35.156 31.142-43.75 64-43.75s64 8.594 64 43.75c0 32.949-12.871 104.179-20.571 132.813zM156.867 288.554c-18.693-18.308-29.958-44.173-28.784-72.599 2.054-49.724 42.395-89.956 92.124-91.881C274.862 121.958 320 165.807 320 220c0 26.827-11.064 51.116-28.866 68.552-2.675 2.62-2.401 6.986.628 9.187 9.312 6.765 16.46 15.343 21.234 25.363 1.741 3.654 6.497 4.66 9.449 1.891 28.826-27.043 46.553-65.783 45.511-108.565-1.855-76.206-63.595-138.208-139.793-140.369C146.869 73.753 80 139.215 80 220c0 41.361 17.532 78.7 45.55 104.989 2.953 2.771 7.711 1.77 9.453-1.887 4.774-10.021 11.923-18.598 21.235-25.363 3.029-2.2 3.304-6.566.629-9.185zM224 0C100.204 0 0 100.185 0 224c0 89.992 52.602 165.647 125.739 201.408 4.333 2.118 9.267-1.544 8.535-6.31-2.382-15.512-4.342-30.946-5.406-44.339-.146-1.836-1.149-3.486-2.678-4.512-47.4-31.806-78.564-86.016-78.187-147.347.592-96.237 79.29-174.648 175.529-174.899C320.793 47.747 400 126.797 400 224c0 61.932-32.158 116.49-80.65 147.867-.999 14.037-3.069 30.588-5.624 47.23-.732 4.767 4.203 8.429 8.535 6.31C395.227 389.727 448 314.187 448 224 448 100.205 347.815 0 224 0zm0 160c-35.346 0-64 28.654-64 64s28.654 64 64 64 64-28.654 64-64-28.654-64-64-64z' />
-              </svg>
-              <span class='sr-only'>Podcast</span>
-            </a>
+            <Podcast class='!w-12 !h-12 bg-blue'></Podcast>
           </li>
           <li>
-            <a class='flex items-center justify-center w-12 h-12 border border-yellow border-4 bg-blue rounded-md p-2'
-               href='/'
-               target='_blank'
-               rel='noopener noreferrer'>
-              <svg xmlns='http://www.w3.org/2000/svg' class='w-full h-full' viewBox='0 0 28 28' fill='currentColor'>
-                <path
-                  d='M11.109 17.625l7.562-3.906-7.562-3.953v7.859zM14 4.156c5.891 0 9.797 0.281 9.797 0.281 0.547 0.063 1.75 0.063 2.812 1.188 0 0 0.859 0.844 1.109 2.781 0.297 2.266 0.281 4.531 0.281 4.531v2.125s0.016 2.266-0.281 4.531c-0.25 1.922-1.109 2.781-1.109 2.781-1.062 1.109-2.266 1.109-2.812 1.172 0 0-3.906 0.297-9.797 0.297v0c-7.281-0.063-9.516-0.281-9.516-0.281-0.625-0.109-2.031-0.078-3.094-1.188 0 0-0.859-0.859-1.109-2.781-0.297-2.266-0.281-4.531-0.281-4.531v-2.125s-0.016-2.266 0.281-4.531c0.25-1.937 1.109-2.781 1.109-2.781 1.062-1.125 2.266-1.125 2.812-1.188 0 0 3.906-0.281 9.797-0.281v0z'></path>
-              </svg>
-              <span class='sr-only'>Youtube</span>
-            </a>
+            <Youtube class='!w-12 !h-12 !border-4 !p-2 bg-blue'></Youtube>
           </li>
-
         </ul>
       </div>
       '
